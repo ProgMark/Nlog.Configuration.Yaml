c75ad55750efcf49e27f2337eea0814b318176e3
Marek Seweryn <marekseweryn@hotmail.com>
2020-08-11 12:38:42 +0200

    added sample project

---
+++ Controllers/LogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Nlog.Configuration.Yaml
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult CreateLogs()
+        {
+            _logger.LogTrace("Example Trace log");
+            _logger.LogDebug("Example Debug log");
+            _logger.LogInformation("Example Information log");
+            _logger.LogWarning("Example Warning log");
+            _logger.LogError("Example Error log");
+            _logger.LogCritical("Example Critical log");
+
+            return Ok();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog.Extensions.Logging;
+
+namespace Nlog.Configuration.Yaml
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddYamlFile("nlogconfig.yaml", true, true);
+            Configuration = builder.Build();
+            NLog.LogManager.Configuration = new NLogLoggingConfiguration(Configuration.GetSection("NLog"));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
